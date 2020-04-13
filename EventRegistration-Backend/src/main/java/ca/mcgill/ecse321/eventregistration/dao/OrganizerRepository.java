3ace12c954582a6d8aec0dba03175bb3f1e10ac5
Helen Kulka <helen.kulka@mail.mcgill.ca>
2020-04-13 17:45:49 -0400

    help

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/OrganizerRepository.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/OrganizerRepository.java
@@ -3,7 +3,6 @@ package ca.mcgill.ecse321.eventregistration.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.eventregistration.model.*;
-import java.util.List;
 
 public interface OrganizerRepository extends CrudRepository<Organizer, String> {
     Organizer findByName(String name);
