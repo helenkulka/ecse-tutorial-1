d0517594b5ef436a90da82529efdbd12ec450979
Helen Kulka <helen.kulka@mail.mcgill.ca>
2020-04-13 19:05:18 -0400

    okok

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/TheatreRepository.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/TheatreRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.eventregistration.model.*;
 
-import java.util.List;
 
 public interface TheatreRepository extends CrudRepository<Theatre, String> {
 
